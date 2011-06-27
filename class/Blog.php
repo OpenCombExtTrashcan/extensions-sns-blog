19ac035b359e396c5705d08f306ccc7c203a2c0b
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-27 05:48:50 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/blog/trunk@43 34e84b91-b3e8-45e1-a0bc-4720add8a18f

---
+++ class/Blog.php
@@ -0,0 +1,19 @@
+<?php
+namespace oc\ext\blog ;
+
+use jc\mvc\model\db\orm\ModelAssociationMap;
+
+use jc\db\DB ;
+use jc\db\PDODriver ;
+
+use oc\ext\Extension;
+
+class Blog extends Extension
+{
+	public function load()
+	{
+	}
+	
+}
+
+?>
