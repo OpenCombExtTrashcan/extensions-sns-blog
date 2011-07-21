f550e0533f73da920cc44e49cbd9df9ed21f6039
huiling <huiling@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-21 06:56:25 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/blog/trunk@312 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- ui/template/compileds/Blog.Index.html.php
+++ ui/template/compileds/Blog.Index.html.php
@@ -1,3 +1,8 @@
+<?php \jc\resrc\HtmlResourcePool::singleton()->addRequire("blog.css",\jc\resrc\HtmlResourcePool::RESRC_CSS) ; ?>
+
+<div class="allblogList">
+<h4>全部博文</h4>
+<ul>
 <?php
 				$__foreach_Arr_var0 = eval("if(!isset(\$__uivar_theModel)){ \$__uivar_theModel=&\$aVariables->getRef('theModel') ;};
 return \$__uivar_theModel->childIterator();");
@@ -6,13 +11,17 @@ return \$__uivar_theModel->childIterator();");
 					foreach($__foreach_Arr_var0 as $__foreach_key_var2 => &$__foreach_item_var1){
 						$__foreach_idx_var3++;
 						 $aVariables->set("row",$__foreach_item_var1 ); ?>
-	
-			<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
-return \$__uivar_row->data('title');") ;?> <a href="?c=blog.update&id=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
-return \$__uivar_row->data('bid');") ;?>">修改</a> <a href="?c=blog.delete&id=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
-return \$__uivar_row->data('bid');") ;?>">删除</a> <br />
-			
+	<li>
+			<span><a href="?c=blog.update&id=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('bid');") ;?>"><?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('title');") ;?></a></span>
+			<em> <a href="?c=blog.update&id=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('bid');") ;?>">编辑</a> <a href="?c=blog.delete&id=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('bid');") ;?>">删除</a></em>
+	</li>		
 <?php 
 					}
 				}
 			 		?>
+</ul>
+</div>
