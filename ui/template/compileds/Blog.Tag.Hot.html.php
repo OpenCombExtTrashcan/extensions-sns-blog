469d93b425c42e63a5a9a657bf2c387df127cc31
huiling <huiling@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-22 02:28:58 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/blog/trunk@332 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- ui/template/compileds/Blog.Tag.Hot.html.php
+++ ui/template/compileds/Blog.Tag.Hot.html.php
@@ -1,4 +1,12 @@
-<?php
+<?php \jc\resrc\HtmlResourcePool::singleton()->addRequire("blog.css",\jc\resrc\HtmlResourcePool::RESRC_CSS) ; ?>
+
+<table cellspacing="0" cellpadding="0" class="blogtagHot">
+	<tbody>
+		<tr class="blogtagHot_tit">
+			<th>标签</th>
+			<th style="width:50%">排名</th>
+		</tr>
+		<?php
 				$__foreach_Arr_var0 = eval("if(!isset(\$__uivar_theModel)){ \$__uivar_theModel=&\$aVariables->getRef('theModel') ;};
 return \$__uivar_theModel->childIterator();");
 				if(!empty($__foreach_Arr_var0)){ 
@@ -6,10 +14,15 @@ return \$__uivar_theModel->childIterator();");
 					foreach($__foreach_Arr_var0 as $__foreach_key_var2 => &$__foreach_item_var1){
 						$__foreach_idx_var3++;
 						 $aVariables->set("row",$__foreach_item_var1 ); ?>
-			<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
-return \$__uivar_row->data('title');") ;?>:排名<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
-return \$__uivar_row->data('hot');") ;?><br />
-<?php 
+		<tr>
+			<td><?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('title');") ;?></td>
+			<td class="hotNumber"><?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('hot');") ;?></td>
+		</tr>
+		<?php 
 					}
 				}
 			 		?>
+	</tbody>
+</table>
